d1e2991f76db30d3870a2430d2065d9c2cf99fe2
LitviniukPetr <0508078344@ukr.net>
2026-06-13 12:38:53 +0300

    Security: implement environment variables for production readiness

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qv1p$7uws#_4p&&)y_mxp$_8*cx8#4mks$4jm_lph&yv+5sa^^'
+import os
+from dotenv import load_dotenv
+
+# Завантажуємо змінні з файлу .env
+load_dotenv(BASE_DIR / '.env')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+# Дозволяємо роботу вашого локального сервера та реального домену
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'uzvs.pp.ua']
 
 
 # Application definition
