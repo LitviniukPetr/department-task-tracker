b5cf8ec67fbebd4b302cb139ef09189ce12554ee
LitviniukPetr <0508078344@ukr.net>
2026-06-13 12:54:29 +0300

    Security: successfully implement environment variables for settings

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
-from dotenv import load_dotenv
 
 # Завантажуємо змінні з файлу .env
 load_dotenv(BASE_DIR / '.env')
